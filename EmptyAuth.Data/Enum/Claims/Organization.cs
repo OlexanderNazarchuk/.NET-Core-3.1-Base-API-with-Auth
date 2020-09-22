da426d901de70302e07167706c8dd26be4490164
Aleksander N <olexander.nazarchuk@gmail.com>
2020-09-22 18:38:48 +0300

    Add data enums

---
+++ EmptyAuth.Data/Enum/Claims/Organization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmptyAuth.Data.Enum.Claims
+{
+	public enum Organization
+	{
+		Create = 1,
+		Read = 2,
+		Update = 3,
+		Delete = 4
+	}
+}
---
+++ EmptyAuth.Data/Enum/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmptyAuth.Data.Enum
+{
+	public enum Role
+	{
+		Admin,
+		OrganizationOwner,
+		PlantOwner
+	}
+}
